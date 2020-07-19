eb5a552b91152f3083440e305ac4c3c2dcd764ce
ejonelunas <eric.jonelunas@gmail.com>
2020-07-18 20:29:23 -0400

    index.jd built out

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import { get } from "http";
+const getInteractions = require("./data");
+const render = require("./image");
+const {getUser} = require("./api");
+const {renderText} = ("./text");
+const Twitter = require("twitter-lite");
+
+dotenv.config();
+
+async function main() {
+    const client = new Twitter({
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+    });
+
+    const bearer = await client.getBearerToken();
+
+    globalThis.TwitterClient = new Twitter({
+        bearer_token: bearer.access_token
+    });
+
+    const user = await getUser("EJonelunas");
+
+    const layers = [9, 16, 27];
+
+    const data = await getInteractions(user.screen_name.toLowercase(), layers);
+
+    await render([
+        {distance: 0, count: 1, radius: 110, users: [user]},
+        {distance: 200, count: layers[0], radius: 64, users: data[0]},
+        {distance: 0, count: layers[1], radius: 58, users: data[1]},
+        {distance: 0, count: layers[2], radius: 50, users: data[2]},
+    ]);
+
+    const shouldRenderText = process.argv.find((arg) => arg ==="--text");
+    if(shouldRenderText) await renderText(data);
+}
+
+main();
